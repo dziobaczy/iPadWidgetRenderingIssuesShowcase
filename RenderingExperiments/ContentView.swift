4fc649c7ee107f4deff87d4a6c798991a19cd442
Piotr Szadkowski <OfftheCode@gmail.com>
2022-07-20 11:19:04 +0200

    Initial Commit

---
+++ RenderingExperiments/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RenderingExperiments
+//
+//  Created by Piotr Szadkowski on 20/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RenderingExperiments/RenderingExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenderingExperimentsApp.swift
+//  RenderingExperiments
+//
+//  Created by Piotr Szadkowski on 20/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct RenderingExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
